90e855f30fe2ad3570cdce2584c269008e5537b5
Tristan Huynh <lictaw@gmail.com>
2024-06-28 09:59:18 +0700

    * Update transformation sound

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,96 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MX_UNI_AMB_MAINTHEME = 3039340560U;
+        static const AkUniqueID PLAY_SX_GAME_AMB_BIOME_PRAIRIEBIRD = 3593667664U;
+        static const AkUniqueID PLAY_SX_GAME_AMB_BIOME_PRAIRIEWIND = 3478365783U;
+        static const AkUniqueID PLAY_SX_GAME_AMB_ENVIRONMENT_WATERSTREAM = 1510396663U;
+        static const AkUniqueID PLAY_SX_GAME_AMB_WEATHER_RAIN = 244388389U;
+        static const AkUniqueID PLAY_SX_GAME_AMB_WEATHER_SNOW = 2821168490U;
+        static const AkUniqueID PLAY_SX_GAME_ENV_SHAKE_START = 2556225634U;
+        static const AkUniqueID PLAY_SX_GAME_INT_BLOCK_DESTROY = 495565927U;
+        static const AkUniqueID PLAY_SX_GAME_INT_BLOCK_GOINGDOWN = 339787471U;
+        static const AkUniqueID PLAY_SX_GAME_INT_BLOCK_MOVE = 4162922556U;
+        static const AkUniqueID PLAY_SX_GAME_INT_BLOCK_ROTATE = 1986295342U;
+        static const AkUniqueID PLAY_SX_GAME_INT_BLOCK_TRANSFORM = 2896509689U;
+        static const AkUniqueID PLAY_SX_GAME_INT_BUBBLE_POP = 2269733319U;
+        static const AkUniqueID PLAY_SX_GAME_UI_CONTROLLER_PRESSDOWNINPUT = 1619009516U;
+        static const AkUniqueID PLAY_SX_UNI_UI_CONTROLLER_MENUNAVIGATION = 2233396232U;
+        static const AkUniqueID PLAY_SX_UNI_UI_CONTROLLER_NEGATIVEACTION = 3133967616U;
+        static const AkUniqueID PLAY_SX_UNI_UI_CONTROLLER_POSITIVEACTION = 2864429968U;
+        static const AkUniqueID PLAY_WEATHER = 3308884970U;
+        static const AkUniqueID STOP_SX_GAME_AMB_WEATHER_RAIN = 3175754591U;
+        static const AkUniqueID STOP_SX_GAME_AMB_WEATHER_SNOW = 1920249428U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace BIOME
+        {
+            static const AkUniqueID GROUP = 835576787U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COASTAL_AREA = 1836997670U;
+                static const AkUniqueID DESERT = 1850388778U;
+                static const AkUniqueID MOUNTAIN = 513139656U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PRAIRIE = 237089847U;
+            } // namespace STATE
+        } // namespace BIOME
+
+        namespace WEATHER
+        {
+            static const AkUniqueID GROUP = 317282339U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID RAIN = 2043403999U;
+                static const AkUniqueID SNOW = 787898836U;
+                static const AkUniqueID SUN = 644904077U;
+            } // namespace STATE
+        } // namespace WEATHER
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SB_BIOME = 3524140603U;
+        static const AkUniqueID SB_BLOCK = 2121512728U;
+        static const AkUniqueID SB_CONTROLLER = 842127849U;
+        static const AkUniqueID SB_MISC = 4077548513U;
+        static const AkUniqueID SB_MUSIC = 779753582U;
+        static const AkUniqueID SB_WEATHER = 2539882731U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BIOME = 835576787U;
+        static const AkUniqueID INTERACTIBLE = 887362671U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID WEATHER = 317282339U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
